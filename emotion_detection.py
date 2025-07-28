6d6ed8d49ae9febe1912666ef6e76c0396566ceb
mcmanusbob <mcmanusbob@hotmail.com>
2025-07-28 18:30:09 -0400

    Add emotion detection function

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyze):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
@@ -6,4 +7,25 @@ def emotion_detector(text_to_analyze):
     input_json = { "raw_document": { "text": text_to_analyze } }
 
     response = requests.post(url, headers=headers, json=input_json)
-    return response.text
+    response_dict = json.loads(response.text)
+
+    emotion_scores = response_dict['emotionPredictions'][0]['emotion']
+
+    anger = emotion_scores.get('anger', 0)
+    disgust = emotion_scores.get('disgust', 0)
+    fear = emotion_scores.get('fear', 0)
+    joy = emotion_scores.get('joy', 0)
+    sadness = emotion_scores.get('sadness', 0)
+
+    emotions = {
+        'anger': anger,
+        'disgust': disgust,
+        'fear': fear,
+        'joy': joy,
+        'sadness': sadness
+    }
+
+    dominant_emotion = max(emotions, key=emotions.get)
+    emotions['dominant_emotion'] = dominant_emotion
+
+    return emotions
